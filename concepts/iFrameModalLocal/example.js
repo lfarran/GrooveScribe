f0b03e2d5433e4f07959462a37847e0e7f0b5a27
Luke Farran <Farran_Luke@shure.com>
2015-12-27 23:48:05 -0600

    If editing an existing groove, the GrooveScribe opens with the groove.  Styling updates. Removed non-angularjs example for now. Fixed iOS9 sizing issue.

--- concepts/iFrameModalLocal/example.js
+++ concepts/iFrameModalLocal/example.js
@@ -1,69 +1,119 @@
 angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
+
 angular.module('ui.bootstrap.demo')
-  .controller('ModalDemoCtrl', function ($scope, $uibModal, $log) {
+  .controller('ModalDemoController', ['$scope', '$uibModal', '$log',
+    function ($scope, $uibModal, $log) {
 
-  //$scope.items = ['item1', 'item2', 'item3'];
+      //$log.debug('ModalDemoController');
+      $scope.open = open;
 
-  //$scope.animationsEnabled = true;
+      $scope.testValue = '{{GrooveTab\n|HasTempo=80\n|HasSwingPercent=0\n|HasDivision=16\n|HasMeasures=1\n|HasNotesPerMeasure=32\n|HasTimeSignature=4/4\n|HasHiHatTab=x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-\n|HasSnareAccentTab=--------O---------------O-------\n|HasSnareOtherTab=--------------------------------\n|HasKickTab=o---------------o---------------\n|HasFootOtherTab=--------------------------------\n|HasTom1Tab=--------------------------------\n|HasTom4Tab=--------------------------------\n|HasEditData=?TimeSig=4/4&Div=16&Title=Two%20Princes%20-%20Verse&Author=Spin%20Doctors&Comments=GrooveDB%20Test&Tempo=104&Measures=8&H=|c-x-x-x-x-x-x-x-|x-x-x-x-x-x-x-x-|o-x-x-x-x-x-x-x-|x-x-x-x-x-x-x-x-|o-x-x-x-x---x-x-|x-x-x-x-x-x-x-x-|o-x-x-x-x-x-x-x-|x-x-x-x-x-x-x---|&S=|-gg-og-g-gg-og-g|-gg-og-g-gg-og-g|-gg-og-g-gg-og-g|-gg-og-g-gg-og-g|-gg-og-g-gg-og-g|-gg-og-g-gg-og-g|-gg-og-g-gg-og-g|-gg-og-g-o-go---|&K=|o-o---oo-oo-----|o-o----o-o------|o-o---oo-oo-----|o-o----o-o------|o-o---oo-oo-----|o-o----o-o------|o-o---oo-oo-----|o-o----o--o-----|&T1=|----------------|----------------|----------------|----------------|----------------|----------------|----------------|-------------x--|&T4=|----------------|----------------|----------------|----------------|----------------|----------------|----------------|--------------xx|&Stickings=|cccccccccccccccc|----------------|----------------|----------------|----------------|----------------|----------------|----------------|\n}}';
 
-  $scope.open = function (/*size*/) {
+      /**
+       * Opens Bootstrap Modal
+       */
+      function open(/*size*/) {
+        var modalInstance = $uibModal.open({
+          animation: true,
+          templateUrl: 'myModalContent.html',
+          controller: 'ModalInstanceController',
+          windowClass: 'center-modal',
+          //size: size,
+          resolve: {
+            grooveUrl: function() {
+              var baseUrl = '../../dist/index.html';
+              var textArea = document.getElementById('sf_free_text');
+              //$log.debug('textArea.value:', textArea.value);
 
-    var modalInstance = $uibModal.open({
-      animation: true,
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      windowClass: 'center-modal',
-      //size: size,
-      resolve: {
-        /*items: function () {
-          return $scope.items;
-        }*/
+              // Groove Data is available
+              if(textArea.value && textArea.value.indexOf('GrooveTab') > -1) {
+                var lines = textArea.value.split('\n');
+
+                lines.forEach(function(line) {
+                  if(line.indexOf('|HasEditData=') > - 1) {
+                    var editData = line.substring('|HasEditData='.length);
+                    //$log.debug('editData:', editData);
+                    baseUrl = baseUrl.concat(editData);
+
+                    // breaking a JavaScript forEach loop requires returning false
+                    // return true or return continue the loop
+                    return false;
+                  }
+                })
+              }
+
+              return baseUrl;
+            }
+          }
+        });
+
+        modalInstance.result
+          // Promise success case
+          .then(function (/*selectedItem*/) {
+            var iFrame = document.getElementById('groove-scribe');
+            //var win = document.getElementById('groove-scribe').contentWindow;
+            var doc = iFrame.contentDocument? iFrame.contentDocument: iFrame.contentWindow.document;
+            var grooveSrc = doc.getElementById('GrooveDB_source');
+            //var abcSrc = doc.getElementById('ABCsource');
+            //$log.debug('iFrame:', iFrame);
+            //$log.debug('win:', win);
+            //$log.debug('doc', doc);
+            //$log.debug('GrooveDB_source:', grooveSrc);
+
+            //$log.debug('grooveSrc.value:', grooveSrc.value);  // drop this into text box
+
+            //$log.debug('abcSrc:', abcSrc);
+            //$log.debug('abcSrc.value:', abcSrc.value);
+
+            var textArea = document.getElementById('sf_free_text');
+            textArea.value = grooveSrc.value;
+
+          }, function () {
+            // Promise error/cancel case
+            $log.info('Modal dismissed at: ' + new Date());
+          });
       }
-    });
 
-    modalInstance.result.then(function (/*selectedItem*/) {
-      //$scope.selected = selectedItem;
+      $scope.$on('$destroy', function () {
+        $log.debug('ModalDemoController destroy');
+      });
 
-      var iFrame = document.getElementById('groove-scribe');
-      //var win = document.getElementById('groove-scribe').contentWindow;
-      var doc = iFrame.contentDocument? iFrame.contentDocument: iFrame.contentWindow.document;
-      var grooveSrc = doc.getElementById('GrooveDB_source');
-      //var abcSrc = doc.getElementById('ABCsource');
-      //$log.debug('iFrame:', iFrame);
-      //$log.debug('win:', win);
-      $log.debug('doc', doc);
-      $log.debug('GrooveDB_source:', grooveSrc);
-      $log.debug('grooveSrc.value:', grooveSrc.value);  // drop this into text box
-      //$log.debug('abcSrc:', abcSrc);
-      //$log.debug('abcSrc.value:', abcSrc.value);
-
-      var textArea = document.getElementById('sf_free_text');
-      textArea.value = grooveSrc.value;
-
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-
-});
+    }]);
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $uibModal service used above.
-
 angular.module('ui.bootstrap.demo')
-  .controller('ModalInstanceCtrl', function ($scope, $uibModalInstance /*, items*/) {
+  .controller('ModalInstanceController', ['$scope', '$uibModalInstance', '$log', 'grooveUrl', '$window',
+    function ($scope, $uibModalInstance, $log, grooveUrl, $window) {
 
-  /*$scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };*/
+      //$log.debug('ModalInstanceController url:', grooveUrl);
+      $log.debug('window height:', $window.innerHeight);
+      $log.debug('window width:', $window.innerWidth);
+      $log.debug('window outerHeight:', $window.outerHeight);
+      $log.debug('window outerWidth:', $window.outerWidth);
 
-  $scope.ok = function () {
-    //$uibModalInstance.close($scope.selected.item);
-    $uibModalInstance.close();
-  };
+      $scope.ok = ok;
+      $scope.cancel = cancel;
+      $scope.grooveScribeUrl = grooveUrl;
+      $scope.windowHeight = $window.innerHeight - 40;
+      //$scope.windowHeight = 650;
+      $scope.windowWidth = $window.innerWidth;
 
-  $scope.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
-  };
-});
+      /**
+       * Bootstrap Modal <OK> button click handler
+       */
+      function ok() {
+        $uibModalInstance.close();
+      }
+
+      /**
+       * Bootstrap Modal <CANCEL> button click handler
+       */
+      function cancel() {
+        $uibModalInstance.dismiss('cancel');
+      }
+
+      $scope.$on('$destroy', function () {
+        $log.debug('ModalInstanceController destroy');
+      });
+    }]);
